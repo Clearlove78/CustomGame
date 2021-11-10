195488ef8bf12c916f8584fd37d8e1f36a4d0fa5
Xia junwe <jxia27@binghamton.edu>
2021-11-10 10:28:36 -0500

    Third commit: add online image resource and next level game page, use button to link them together

--- app/src/main/java/com/example/customgame/ThirdActivity.java
+++ app/src/main/java/com/example/customgame/ThirdActivity.java
@@ -1,17 +1,30 @@
 package com.example.customgame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class ThirdActivity extends Activity {
     private VideoView videoView;
+    private Button btn_start,btn_end;
     @Override
-    protected  void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
+        music.play(this, R.raw.hua);
+        Button btn2=(Button) findViewById(R.id.button3);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ThirdActivity.this,End.class) ;
+                startActivity(intent);
+            }
+        });
     }
 }
